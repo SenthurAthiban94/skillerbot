8d152abfd2e4eb7bf05be11b2ce0d9d52aa69e44
senthur <senthur@financebuddha.com>
2018-03-26 17:36:04 +0530

    New Google Credentials for signup to askskillerbot.gmail.com

--- config/keys.js
+++ config/keys.js
@@ -1,13 +1,15 @@
 //add to .gitignore
 
-
+//Previous Id
+//Client Id = 37483636987-l2uv4oa5dp76h9r3bo6eqoortk6f88ig.apps.googleusercontent.com
+// ClientSecret =80932UL5zQKweMX-6kFud0H8;
 module.exports = {
     mongodb:{
         dbURL:'mongodb://mongodb:test@ds161346.mlab.com:61346/linkedin-data'
     },
     google:{
-        clientID:'37483636987-l2uv4oa5dp76h9r3bo6eqoortk6f88ig.apps.googleusercontent.com',
-        clientSecret:'80932UL5zQKweMX-6kFud0H8'
+        clientID:'948827112863-vjiorbjlmbpce0585huhv3ofnrg9upf5.apps.googleusercontent.com',                
+        clientSecret:'i71HgsRpM36-euzVj4WdYknR'
     },
     linkedIn:{
         clientID:'81osj02dqytkh8',
